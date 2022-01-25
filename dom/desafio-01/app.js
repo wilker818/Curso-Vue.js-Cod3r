7dc9663384528084dcb9819ff10c5988bb7984af
wilker818 <wilkerferreira818@gmail.com>
2022-01-24 22:04:35 -0300

    curso vue-js

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#desafio",
+    data: {
+        Nome: "Wilker Ferreira",
+        idade: "25",
+        link: "https://wallpapercave.com/wp/wp6878944.jpg",
+        linkHtml: "<a href='http://google.com.br'>Google v-html</a>",
+    },
+    methods: {
+        saudacao: function () {
+
+        },
+    },
+});
